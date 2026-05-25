017f3e6664dda5698915aeee4efcaa0475e437de
kimzz14 <kimzz1004@gmail.com>
2026-05-25 10:24:16 +0900

    init

---
+++ 00.export_path.sh
@@ -0,0 +1,3 @@
+conda activate PopLDdecay_v3.43_260525
+
+#conda create -n PopLDdecay_v3.43_260525 poplddecay
---
+++ pipe/run_chr.sh
@@ -0,0 +1,5 @@
+PopLDdecay \
+    -InVCF  <in.vcf.gz>  \
+    -OutStat <out.stat> \
+    -MaxDist 10000 \
+    
